5a11b0d6fce0412f6144620652971ab7c19743b2
tuyendt <tuyendt@itsol.vn>
2022-03-15 11:39:45 +0700

    add consumer code

--- src/com/sample/ConsumerSample.java
+++ src/com/sample/ConsumerSample.java
@@ -1,5 +1,6 @@
 package com.sample;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
@@ -18,5 +19,48 @@ public class ConsumerSample {
                 Arrays.asList(1,2,3,4,5),
                 (Integer i) -> System.out.println(i)
         );
+
+        // Consumer to display a number
+        Consumer<Integer> display = a -> System.out.println(a);
+
+        // Implement display using accept()
+        display.accept(10);
+
+        // Consumer to multiply 2 to every integer of a list
+        Consumer<List<Integer> > modify = list ->
+        {
+            for (int i = 0; i < list.size(); i++)
+                list.set(i, 2 * list.get(i));
+        };
+
+        // Consumer to display a list of numbers
+        Consumer<List<Integer> >
+                dispList = list -> list.stream().forEach(a -> System.out.print(a + " "));
+
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(2);
+        list.add(1);
+        list.add(3);
+
+        // Implement modify using accept()
+        modify.accept(list);
+        System.out.println();
+
+        // Implement dispList using accept()
+        dispList.accept(list);
+        System.out.println();
+
+        // using andThen()
+        modify.andThen(dispList).accept(list);
+        System.out.println();
+
+
+        try {
+            // using andThen() return NullPointerEx
+            modify.andThen(null).accept(list);
+        }
+        catch (Exception e) {
+            System.out.println("Exception: " + e);
+        }
     }
 }
